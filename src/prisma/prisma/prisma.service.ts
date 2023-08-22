11259c3404bd469ee8c3016df82d1ae8850117d9
smarcosm <sebastiaomarcos49@gmail.com>
2023-08-22 14:56:20 -0300

    chore: :wrench: Db configurado

--- src/prisma/prisma/prisma.service.ts
+++ src/prisma/prisma/prisma.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaClient extends PrismaClient implements OnModuleInit{}
